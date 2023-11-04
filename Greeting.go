3d60ff98fb92949fd410a07c6e3fa4f5d325222b
himitoyadama <119666780+himitoyadama@users.noreply.github.com>
2023-11-05 03:27:01 +0900

    Create Greeting.go

---
+++ Greeting.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Good morning")
+
+	fmt.Println("Good afternoon")
+
+	fmt.Println("Good evening")
+}
+
+//TERMINAL
+> go build Greeting.go
+> ./Greeting
