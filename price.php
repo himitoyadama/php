26af7d712783633b1850df5742a179af5df2beeb
himitoyadama <119666780+himitoyadama@users.noreply.github.com>
2024-01-12 05:23:31 +0900

    Update price.php

--- price.php
+++ price.php
@@ -14,3 +14,18 @@ echo "リンゴを買う数:".$apple_num."個";
 ?>
 リンゴの値段:350円
 リンゴを買う数:5個
+
+<?php
+// 値段計算をしてみよう
+// 代数演算子 + - * / %
+// $apple　リンゴの値段
+// $apple_num　リンゴを買う数
+$apple = 350;
+$apple_num = rand(1,10); //rand関数
+
+//改行 \n
+echo "リンゴの値段:".$apple."円\n";
+echo "リンゴを買う数:".$apple_num."個\n";
+$total = $apple * $apple_num;
+echo "合計".$total."円";  //例：8個なら合計2800円
+?>
