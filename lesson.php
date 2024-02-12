3f54365b94a2441676bd90355203b6a93e3b03fe
himitoyadama <119666780+himitoyadama@users.noreply.github.com>
2024-02-13 07:02:06 +0900

    Update lesson.php

--- lesson.php
+++ lesson.php
@@ -1,5 +1,29 @@
 <?php
+$a = 10;
+$b = 1;
+$bool01 = ($a > $b);
 
+echo $bool01. "\n";
+echo gettype($bool01); 
+//terminal
+ C:\xampp\htdocs\lesson> php lesson.php
+1
+boolean
+//$bool01 = ($a < $b);はbooleanのみ表示  
+   
+<?php
+$string_a = "hello fuck!";
+
+echo $string_a. "\n";
+echo gettype($string_a);
+
+//terminal
+C:\xampp\htdocs\lesson> php lesson.php
+hello fuck!
+string
+
+
+<?php
 $fuck1 = 123;
 $fuck2 = 1.23;
 
@@ -12,3 +36,5 @@ C:\xampp\htdocs\lesson> php lesson.php
 integer
 double
 float(1.23)  
+
+  
