646f8299f04c709fecf4c79a4a6027180cf5ed35
himitoyadama <119666780+himitoyadama@users.noreply.github.com>
2024-02-13 07:18:15 +0900

    Create lesson2.php

---
+++ lesson2.php
@@ -0,0 +1,16 @@
+<?php
+//配列の要素数=array(3)
+$a = array(3);
+$a[0] = "sato";
+$a[1] = "suzuki";
+$a[2] = "taahashi";
+
+echo ($a[0])."\n";
+echo ($a[1])."\n";
+echo ($a [2])."\n";
+
+//terminal
+C:\xampp\htdocs\lesson> php lesson2.php
+sato
+suzuki
+taahashi
