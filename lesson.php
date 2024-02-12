6bb1d1be1416ebb527c2748e8bc34fe7610fd544
himitoyadama <119666780+himitoyadama@users.noreply.github.com>
2024-02-13 06:41:18 +0900

    Create lesson.php

---
+++ lesson.php
@@ -0,0 +1,14 @@
+<?php
+
+$fuck1 = 123;
+$fuck2 = 1.23;
+
+echo gettype($fuck1) . "\n";
+echo gettype($fuck2) . "\n";
+var_dump($fuck2);
+
+//terminal
+C:\xampp\htdocs\lesson> php lesson.php
+integer
+double
+float(1.23)  
